29f48ecdbf916fbf0796abc13be23e4d6b5da0b3
alumno <alumno@localhost.localdomain>
2017-04-27 12:57:44 +0200

    clase

--- src/cliente/cliente.java
+++ src/cliente/cliente.java
@@ -1,7 +1,7 @@
 /*
  * Programa cliente del servidor
  */
-package cliente;
+
 import java.io.*;
 import java.net.*;
 /**
@@ -9,14 +9,29 @@ import java.net.*;
  * @author Mario
  */
 public class cliente {
-    private final static String HOST = "localhost";
+    private static String HOST = "localhost";
     private final static int PUERTO = 2018;
     public static void main(String args[]) {
          
         Socket socketComunicacion;  //Para hablar con alguien
         BufferedReader entrada;
         DataOutputStream salida; //Los mensajitos
-        System.out.println("SOY EL CLIENTE");
+		String cadena;		
+		if(args.length==2)  //java cliente 192.168.1.169 hola que pasa
+		{
+			HOST=args[0];
+			cadena=args[1];
+		}
+		else if(args.length==1){	// java cliente 192.168.1.169
+			cadena="No tengo nada que decir";
+			HOST=args[0];			
+		}
+		else{
+			cadena="No tengo nada que decir";
+		}	
+			        
+		
+		System.out.println("SOY EL CLIENTE");
          
         try {
             //Creamos nuestro socket
@@ -28,7 +43,8 @@ public class cliente {
                      socketComunicacion.getOutputStream());
             salida.flush();
             //Enviamos un mensaje
-            salida.writeUTF("¿Hay alguien?\n");
+            salida.writeUTF(cadena+"\n");
+			System.out.println("aqui llego");
             String mensaje = entrada.readLine();
             System.out.println("Nos dicen: "+mensaje);
             //Cerramos la conexión
--- src/servidor/servidor.java
+++ src/servidor/servidor.java
@@ -1,9 +1,10 @@
 /*
  * Programa para crear un servidor
  */
-package servidor;
+
 import java.io.*;
 import java.net.*;
+import java.time.*;
 
 /**
  * Clase servidor para montarse un pequeño escuchante.
@@ -23,34 +24,52 @@ public class servidor {
         try{
             socketEscuchante = new ServerSocket(PUERTO);
             System.out.println("Esperando un cliente...");
-            socketComunicacion = socketEscuchante.accept();
-            System.out.println("Alguien llamó...");
+			while(true){            
+				socketComunicacion = socketEscuchante.accept();
+	            System.out.println("Alguien llamó...");
             
-            // Establecemos los canales de entrada y salida
+    	        // Establecemos los canales de entrada y salida
             
-            entrada = new BufferedReader(new InputStreamReader(
-                        socketComunicacion.getInputStream()));
-            salida = new DataOutputStream(
-                     socketComunicacion.getOutputStream());
+    	        entrada = new BufferedReader(new InputStreamReader(
+    	                    socketComunicacion.getInputStream()));
+    	        salida = new DataOutputStream(
+    	                   socketComunicacion.getOutputStream());
             
-            // Chachi ya hemos establecido los canales de comunicacion
+          		  // Chachi ya hemos establecido los canales de comunicacion
             
-            System.out.println(" Conectado con: "
+          		System.out.println(" Conectado con: "
                     + socketComunicacion.getInetAddress());
-            salida.flush();
-            String mensaje = entrada.readLine();
-            System.out.println(socketComunicacion.getInetAddress()
-                    +" dice: "+mensaje);
-            
-            salida.writeUTF("Hola aquí el servidor a sus pies.\n");
-            
-            System.out.println("Confirmando al cliente");
-            //Cuelga tu, no tu, no tu
-            System.out.println("Cerrando la conexión...");
-            socketEscuchante.close();
+           		salida.flush();
+	            String mensaje = entrada.readLine();
+    	        System.out.println(socketComunicacion.getInetAddress()
+    	                +" dice: "+mensaje);
+            	
+				String mensajeSalida="Comando u orden incorrecta";
+				if(mensaje.endsWith("SKYNET")){
+					mensajeSalida="Mañana destruiré el mundo";
+				}else if(mensaje.endsWith("DATE")){
+					mensajeSalida=LocalDate.now().toString();
+				}else if(mensaje.endsWith("NOMBRE")){
+					mensajeSalida="Yo soy yo";
+				}
+				
+				System.out.println("Mensaje de respuesta:"+mensajeSalida);
+		        salida.writeUTF(mensajeSalida+"\n");
+
+				
+		        
+		        System.out.println("Confirmando al cliente");
+		        //Cuelga tu, no tu, no tu
+		        System.out.println("Cerrando la conexión...");
+				entrada.close();
+				salida.close();
+				socketComunicacion.close();
+			}		
+			//socketEscuchante.close();
             
         }catch(IOException io){
             System.err.println("Error en la comunicacion "+io.getMessage());
+			//socketEscuchante.close();
         }
     }
 
